548a9b168ff929ccecfb72fac2603855a599e612
chemaespinoza <55041878+chemaespinoza@users.noreply.github.com>
2019-11-06 17:00:52 -0800

    Add files via upload

---
+++ examen/rectangulo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <?php
+    echo"ingresa su base"$base
+    echo"ingresa su altura"$altura
+    echo "el area del rectangulo es ".rectangulo($basebase, $altura)."<br>";
+    function rectangulo($base, $altura){
+        $area = $base*$altura; 
+        return $area;
+       }
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
